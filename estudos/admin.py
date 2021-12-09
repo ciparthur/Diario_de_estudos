aa389f2b8d4b67bf3e3de1542b5c977b497fa2df
Arthur Gabriel Cipitelli <cipitelliarthurgabriel@protonmail.com>
2021-12-08 22:12:36 -0300

    Criação dos modelos Topico e Entrada

--- estudos/admin.py
+++ estudos/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Topico, Entrada
+
+admin.site.register(Topico)
+admin.site.register(Entrada)
--- estudos/models.py
+++ estudos/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+
+class Topico(models.Model):
+    texto = models.CharField(max_length=200)
+    data_pub = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolve uma representação em strings do modelo."""
+        return self.texto
+
+
+class Entrada(models.Model):
+    topico = models.ForeignKey(Topico, on_delete=models.CASCADE)
+    texto = models.TextField()
+    data_entrada = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolve uma representação em strings do modelo."""
+        if len(self.texto) < 50:
+            return self.texto
+        else:
+            return f'{self.texto[:50]}...'
