504cbd6f40aa9fd47b3aeb280740881e93b2bbfa
Arthur Gabriel Cipitelli <cipitelliarthurgabriel@protonmail.com>
2021-12-09 11:42:16 -0300

    Página de tópicos criada

--- diario/views.py
+++ diario/views.py
@@ -1,5 +1,15 @@
 from django.shortcuts import render
 
+from .models import Topico
+
 def index(request):
     """Página inicial"""
     return render(request, 'diario/index.html')
+
+
+def topicos(request):
+    """Página de tópicos"""
+    topicos = Topico.objects.order_by('-data_pub')
+    contexto = {'topicos': topicos}
+
+    return render(request, 'diario/topicos.html', contexto)
