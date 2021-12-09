25629c62860d51145f01802294b187b2b1acb082
Arthur Gabriel Cipitelli <cipitelliarthurgabriel@protonmail.com>
2021-12-09 11:17:50 -0300

    Criação dos modelos 'Topico' e 'Entrada'

--- diario/models.py
+++ diario/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Topico(models.Model):
+    """Criação dos tópicos."""
+    texto = models.CharField(max_length=200)
+    data_pub = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolve o nome do tópico em strings."""
+        return self.texto
+
+
+class Entrada(models.Model):
+    """Criação dos assuntos e relações com os tópicos."""
+    topico = models.ForeignKey(Topico)
+    texto = models.TextField()
+    data_ent = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        """Devolve o assunto em strings."""
+        if len(self.texto) < 50:
+            return self.texto
+        else:
+            return self.texto[:50]
