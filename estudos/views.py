80f45b92701f38b18388e38c0649dd9306580527
Arthur Gabriel Cipitelli <cipitelliarthurgabriel@protonmail.com>
2021-12-08 22:22:09 -0300

    Criação da página tópicos

--- estudos/views.py
+++ estudos/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+from .models import Topico
+
 def index(request):
     """Página inicial do projeto."""
     return render(request, 'estudos/index.html')
+
+
+def topicos(request):
+    topicos = Topico.objects.order_by('-data_pub')
+    contexto = {'topicos': topicos}
+
+    return render(request, 'estudos/topicos.html', contexto)
