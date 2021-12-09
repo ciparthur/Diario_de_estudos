8451d07657653d2d679102013852d2f08ec0c7d3
Arthur Gabriel Cipitelli <cipitelliarthurgabriel@protonmail.com>
2021-12-09 10:58:15 -0300

    Alteração do nome do app 'estudos' para 'diario'

--- diario/apps.py
+++ diario/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class EstudosConfig(AppConfig):
+class DiarioConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'estudos'
+    name = 'diario'
--- diario_de_estudos/settings.py
+++ diario_de_estudos/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     # Minhas aplicações
-    'estudos.apps.EstudosConfig'
+    'diario.apps.DiarioConfig'
 ]
 
 MIDDLEWARE = [
