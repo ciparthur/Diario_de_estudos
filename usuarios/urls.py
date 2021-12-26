9349ff6bb01afbb23bec07591aa5d1adf4405b1c
Arthur <cipitelliarthurgabriel@protonmail.com>
2021-12-26 09:15:39 -0300

    Criação da página de login

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -1,5 +1,8 @@
 from django.urls import path
+from django.contrib.auth.views import LoginView
+
+from . import views
 
 urlpatterns = [
-    
+    path('login/', LoginView.as_view(), {'template_name': 'usuarios/login.html'}, name='login')
 ]
