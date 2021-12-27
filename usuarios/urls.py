2c89d0890e82da88028a8669fc0beb8960ff7aea
Arthur <cipitelliarthurgabriel@protonmail.com>
2021-12-27 13:58:37 -0300

    Update urls.py

--- usuarios/urls.py
+++ usuarios/urls.py
@@ -4,7 +4,7 @@ from django.contrib.auth.views import LoginView
 from . import views
 
 urlpatterns = [
-    path('login/', LoginView.as_view(), {'template_name': 'registration/login.html'}, name='login'),
+    path('login/', LoginView.as_view(), name='login'),
     path('logout/', views.logout_view, name='logout'),
     path('cadastro/', views.cadastro, name='cadastro')
 ]
